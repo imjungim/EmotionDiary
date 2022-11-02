c14aa14adf3aced6c3bae63b26cc6229eae0ee84
imjungim <jungim4201@gmail.com>
2022-11-02 13:56:06 +0900

    emotion filter

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const filterOptionList = [
+  {value : 'all', name : '전부다'},
+  {value : 'good', name : '좋은 감정만'},
+  {value : 'bad', name : '안 좋은 감정만'},
+]
+
 //정렬 컴포넌트
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
@@ -20,10 +26,19 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
-
+  const [filter, setFilter] = useState('all');
   //정렬함수
   const getProcessedDiaryList = () => {
     //sort 사용시 원본배열 자체가 정렬되어 바뀜 -> 깊은 복사로
+
+    const filterCallBack = (item) => {
+      if(filter === 'good'){
+        return parseInt(item.emotion) <= 3;
+      }else{
+        return parseInt(item.emotion) > 3;
+      }
+    }
+
     const compare = (a, b) => {
       if(sortType === 'latest'){
         return parseInt(b.date)- parseInt(a.date);
@@ -32,7 +47,9 @@ const DiaryList = ({ diaryList }) => {
       }
     }
     const copyList = JSON.parse(JSON.stringify(diaryList)) //원본배열(diaryList) =>문자열로 변환 => 다시 배열
-    const sortedList = copyList.sort(compare);
+
+    const filteredList = filter === 'all' ? copyList : copyList.filter((it)=>filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   }
 
@@ -43,8 +60,14 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>{it.content} {it.emotion}</div>
       ))}
     </div>
   );
