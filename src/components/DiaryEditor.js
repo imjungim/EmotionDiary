6bf769fb7acaf4d0ecd1d01a8c25b2edc74bdc0a
imjungim <jungim4201@gmail.com>
2022-11-03 23:48:56 +0900

    일기 상세 페이지 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useState, useRef, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
 import EmotionItem from "./EmotionItem";
@@ -39,13 +39,14 @@ const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
 };
 
-const DiaryEditor = () => {
+
+const DiaryEditor = ({isEdit, originData}) => {
   const contentRef = useRef();
   const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const {onCreate} = useContext(DiaryDispatchContext);
+  const {onCreate, onEdit} = useContext(DiaryDispatchContext);
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
@@ -56,17 +57,32 @@ const DiaryEditor = () => {
       contentRef.current.focus();
       return;
     }
+    if(window.confirm(isEdit? "일기를 수정하시겠습니까? " : "새로운 일기를 작성하시겠습니까?")){
+      if(!isEdit){
+        onCreate(date, content, emotion);
+      }else{
+        onEdit(originData.id, date, content, emotion)
+      }
+    }
+
     //app컴포넌트에 data에 일기 데이터를 추가 -> oncreate함수
     onCreate(date, content, emotion);
     navigate('/',{replace:true})
   }
 
+  useEffect(()=>{
+    if(isEdit){
+      setDate(getStringDate(new Date(parseInt(originData.date))))
+      setEmotion(originData.emotion)
+      setContent(originData.content)
+    }
+  },[isEdit,originData])
 
 
   return (
     <div className="DiaryEditor">
       <MyHeader
-        headText={"새 일기쓰기"}
+        headText={isEdit? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
         }
