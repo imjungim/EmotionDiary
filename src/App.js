8916c88a413b4fffbc1164397e70dd7f6bd6bcea
imjungim <jungim4201@gmail.com>
2022-11-07 19:49:13 +0900

    LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, json } from "react-router-dom";
 
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -34,48 +34,45 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+  localStorage.setItem('diary', JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1667360344621,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: "오늘의 일기 2번",
-    date: 1667360344622,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: "오늘의 일기 3번",
-    date: 1667360344623,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: "오늘의 일기 4번",
-    date: 1667360344624,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: "오늘의 일기 5번",
-    date: 1667360344625,
-  },
-];
+
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
-  const dataId = useRef(6);
+
+  //localStorage 사용 실습
+  // useEffect(()=>{
+  //   // localStorage.setItem("item1", 10);
+  //   // localStorage.setItem("item2", 20);
+  //   // localStorage.setItem("item3", JSON.stringify({ value : 30 }));
+  //   const item1 = localStorage.getItem('item1');
+  //   const item2 = localStorage.getItem('item2');
+  //   const item3 = JSON.parse(localStorage.getItem('item3'));
+  //   console.log({item1, item2, item3}) //문자열로 바뀌어 나옴.(number type ->string type)
+  // },[]) 
+
+
+
+
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(()=>{
+    const localData = localStorage.getItem('diary');
+    if(localData){
+      const diaryList = JSON.parse(localData).sort((a,b)=>parseInt(b.id)-parseInt(a.id));
+      dataId.current =parseInt(diaryList[0].id) + 1
+
+      //diaryList 를data 초기값으로 설정
+      dispatch({type : 'INIT', data : diaryList});
+    }
+  },[])
+
+  const dataId = useRef(0);
 
   //create
   const onCreate = (date, content, emotion) => {
--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,59 +1,74 @@
 import React, { useState, useRef, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
-import { getStringDate } from "../util/data"; 
+import { getStringDate } from "../util/data";
 import { emotionList } from "../util/emotion";
 import EmotionItem from "./EmotionItem";
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 
-
-
-
-const DiaryEditor = ({isEdit, originData}) => {
+const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
   const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const {onCreate, onEdit} = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
   };
   const navigate = useNavigate();
   const handleSubmit = () => {
-    if(content.length < 1 ) {
+    if (content.length < 1) {
       contentRef.current.focus();
       return;
     }
-    if(window.confirm(isEdit? "일기를 수정하시겠습니까? " : "새로운 일기를 작성하시겠습니까?")){
-      if(!isEdit){
-        onCreate(date, content, emotion);    //app컴포넌트에 data에 일기 데이터를 추가 -> oncreate함수
-      }else{
-        onEdit(originData.id, date, content, emotion)
+    if (
+      window.confirm(
+        isEdit ? "일기를 수정하시겠습니까? " : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion); //app컴포넌트에 data에 일기 데이터를 추가 -> oncreate함수
+      } else {
+        onEdit(originData.id, date, content, emotion);
       }
     }
 
+    navigate("/", { replace: true });
+  };
 
-    navigate('/',{replace:true})
-  }
-
-  useEffect(()=>{
-    if(isEdit){
-      setDate(getStringDate(new Date(parseInt(originData.date))))
-      setEmotion(originData.emotion)
-      setContent(originData.content)
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
     }
-  },[isEdit,originData])
+  };
 
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
 
   return (
     <div className="DiaryEditor">
       <MyHeader
-        headText={isEdit? "일기 수정하기" : "새 일기쓰기"}
+        headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
         }
       />
       <div>
@@ -95,7 +110,11 @@ const DiaryEditor = ({isEdit, originData}) => {
         <section>
           <div className="control_box">
             <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
-            <MyButton text={"작성완료"} type={"positive"} onClick={handleSubmit} />
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={handleSubmit}
+            />
           </div>
         </section>
       </div>
